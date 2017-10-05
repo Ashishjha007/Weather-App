32acbec5585f80932d1c27c21334ea3fb011a2eb
Ashish jha <jha.ashish152@gmail.com>
2017-10-05 14:23:55 +0530

    Formatted WeatherDataSource class

--- Weather/DataSource/WeatherDataSource.swift
+++ Weather/DataSource/WeatherDataSource.swift
@@ -11,16 +11,11 @@ import Foundation
 func fetchWeatherReport (_ location: String, callback: @escaping (NSDictionary?) -> ()) {
 
     let apiKey = "19c994d2ac36e22d2a9a6fcf9e3cd788"
-
     let foresastURL = URL(string: "https://api.forecast.io/forecast/" + apiKey + "/" + location)
-
     let sharedSession = URLSession.shared
-
     let downloadTask: URLSessionDownloadTask = sharedSession.downloadTask(with: foresastURL!, completionHandler: { (location, response, error) -> Void in
-
         var weatherDataDictionary: NSDictionary?
         if (error == nil) {
-
             let dataObject = try? Data(contentsOf: location!)
             weatherDataDictionary = (try! JSONSerialization.jsonObject(with: dataObject!, options: [])) as? NSDictionary
         }
